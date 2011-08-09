5cd202a0077f9e93cb2d6e28169161f067d91da9
Peter Danenberg <pcd@roxygen.org>
2011-08-09 01:31:26 -0700

    Regex experiments

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
-(defproject hangman "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+(defproject hangman "0.1-SNAPSHOT"
+  :description "Hangman"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.3.2"]])
