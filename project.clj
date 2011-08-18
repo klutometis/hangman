d61d96ad62c7d98ace8b6bcd530b03ef0d2072c2
Peter Danenberg <pcd@roxygen.org>
2011-08-18 02:02:00 -0700

    Remove delimc.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject hangman "0.1-SNAPSHOT"
   :description "Hangman"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [delimc "0.1.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :source-path [["src/clojure"]]
   :java-source-path [["src/java"]]
   :dev-dependencies [[swank-clojure "1.3.2"]])
