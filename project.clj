0ca6f45898da57c5a9cb4fc56c92196a050807d5
Peter Danenberg <pcd@roxygen.org>
2011-08-18 01:58:00 -0700

    Experiment with delimited continuations

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hangman "0.1-SNAPSHOT"
   :description "Hangman"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :java-source-path [["src"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [delimc "0.1.0"]]
   :dev-dependencies [[swank-clojure "1.3.2"]])
