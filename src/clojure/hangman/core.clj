155c95fc8570b7b8b04f54be8232082fdf68fe08
Peter Danenberg <pcd@roxygen.org>
2011-08-19 05:00:03 -0700

    Abstract some mechanical things into hangman.core.

--- src/clojure/hangman/core.clj
+++ src/clojure/hangman/core.clj
@@ -1,1 +1,18 @@
-(ns hangman.core)
+(ns hangman.core
+  (:import (com.factual.hangman
+            HangmanGame$Status)))
+
+(defn- can-keep-guessing?
+  ([game] (= (.gameStatus game) HangmanGame$Status/KEEP_GUESSING)))
+
+(defn run
+  ([game strategy]
+     ;; Can also just wrap this in with-open, since
+     ;; assertCanKeepGuessing (in guessLetter and guessWord) will
+     ;; throw an exception.
+     (while (can-keep-guessing? game)
+       (println game)
+       (let [guess (.nextGuess strategy game)]
+         (.makeGuess guess game)))
+     (println game)
+     (.currentScore game)))
