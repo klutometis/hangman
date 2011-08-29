41fb186d7387e1c0fbeff47ba64e3d4fbf30c57b
Peter Danenberg <pcd@roxygen.org>
2011-08-28 23:40:19 -0700

    Some experiments with continuations and word-counting; arity->trie-dictionary

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
                  [commons-collections "3.2.1"]]
   :source-path "src/clojure"
   :java-source-path "src/java"
-  :dev-dependencies [[swank-clojure "1.3.2"]])
+  :dev-dependencies [[swank-clojure "1.3.2"]
+                     [delimc "0.1.0"]
+                     [net.dnolen.clj-cont "0.1.0"]])
