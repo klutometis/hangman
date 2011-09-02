8f4794cf7a1b88745275c916b04bdc51830fa496
Peter Danenberg <pcd@roxygen.org>
2011-09-01 21:11:43 -0700

    Testing run-words

--- src/clojure/hangman/core.clj
+++ src/clojure/hangman/core.clj
@@ -1,7 +1,10 @@
 (ns hangman.core
   (:import (com.factual.hangman
+            HangmanGame
             HangmanGame$Status))
-  (:use clojure.contrib.pprint))
+  (:use clojure.contrib.pprint
+        hangman.frequency-strategy
+        hangman.regex-strategy))
 
 (defmacro debug-map
   [& exprs]
@@ -18,14 +21,47 @@
 (defn- can-keep-guessing?
   ([game] (= (.gameStatus game) HangmanGame$Status/KEEP_GUESSING)))
 
-(defn run
-  ([game strategy]
-     ;; Can also just wrap this in with-open, since
-     ;; assertCanKeepGuessing (in guessLetter and guessWord) will
-     ;; throw an exception.
-     (while (can-keep-guessing? game)
-       (println game)
-       (let [guess (.nextGuess strategy game)]
-         (.makeGuess guess game)))
+(defmacro time-and-value [expr]
+  `(let [start# (. System (nanoTime))
+         ret# ~expr]
+     [(/ (double (- (. System (nanoTime)) start#)) 1000000.0)
+      ret#]))
+
+(defn run [game strategy]
+  ;; Can also just wrap this in with-open, since
+  ;; assertCanKeepGuessing (in guessLetter and guessWord) will
+  ;; throw an exception.
+  (while (can-keep-guessing? game)
+    (println game)
+    (let [guess (.nextGuess strategy game)]
+      (.makeGuess guess game)))
      (println game)
-     (.currentScore game)))
+     (.currentScore game))
+
+(defn average-vals [data key]
+  (let [vals (map key (vals data))]
+    (float (/ (reduce + vals) (count vals)))))
+
+(defn run-words
+  [make-strategy
+   arity->dictionary
+   arity->letter->count
+   words]
+  (let [time-values
+        (map (fn [word]
+               (let [arity (count word)
+                     [time value]
+                     (time-and-value
+                      (run (new HangmanGame word 4)
+                           (make-strategy
+                            (arity->dictionary arity)
+                            (arity->letter->count arity))))]
+                 {:time time
+                  :score value}))
+             words)
+        data (zipmap words time-values)
+        average-time (average-vals data :time)
+        average-score (average-vals data :score)]
+    {:data (into (sorted-map) data)
+     :average-time average-time
+     :average-score average-score}))
